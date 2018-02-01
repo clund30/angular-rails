e713a60ed887733d29cee698b64cc12ef8cf3cfb
Casey Lund <caseylund21@gmail.com>
2018-02-01 15:51:33 -0700

    Added proposals section and updated nav bar

--- freelance-camp-fe/app/app-routing.module.ts
+++ freelance-camp-fe/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'documents', component: DocumentsComponent },
+  { path: 'proposals', component: ProposalListComponent },
 ]
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ]
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {};
