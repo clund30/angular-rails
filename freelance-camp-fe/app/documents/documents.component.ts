037542c2d4362b1338e7aad4d6ac8d78d6d7e3d9
Casey Lund <caseylund21@gmail.com>
2018-02-01 15:30:09 -0700

    created mock data for documents page

---
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Document } from './document';
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+export class DocumentsComponent {
+  documents: Document[] = [
+  {
+    title: "My first doc",
+    description: 'this is my first doc',
+    file_url: 'http://google.com',
+    updated_at: '02/01/18',
+    image_url: 'http://google.com'
+  },
+  {
+    title: "My Second doc",
+    description: 'this is my first doc',
+    file_url: 'http://google.com',
+    updated_at: '02/01/18',
+    image_url: 'http://google.com'
+  },
+  {
+    title: "My Third doc",
+    description: 'this is my first doc',
+    file_url: 'http://google.com',
+    updated_at: '02/01/18',
+    image_url: 'http://google.com'
+  }
+  ]
+}
