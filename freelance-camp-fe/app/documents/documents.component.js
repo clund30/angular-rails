12cc2aab7e14e04ab85f3d0da021b319a94fe061
Casey Lund <caseylund21@gmail.com>
2018-02-05 11:01:42 -0700

    Added styles

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -12,25 +12,25 @@ var DocumentsComponent = /** @class */ (function () {
         this.pageTitle = "Document Dashboard";
         this.documents = [
             {
-                title: "My first doc",
-                description: 'this is my first doc',
+                title: "My First Doc",
+                description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
-                updated_at: '02/01/18',
-                image_url: 'http://google.com'
+                updated_at: '11/11/16',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             },
             {
-                title: "My Second doc",
-                description: 'this is my first doc',
+                title: "My Second Doc",
+                description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
-                updated_at: '02/01/18',
-                image_url: 'http://google.com'
+                updated_at: '11/11/16',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             },
             {
-                title: "My Third doc",
-                description: 'this is my first doc',
+                title: "My Last Doc",
+                description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
-                updated_at: '02/01/18',
-                image_url: 'http://google.com'
+                updated_at: '11/11/16',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             }
         ];
     }
@@ -38,7 +38,8 @@ var DocumentsComponent = /** @class */ (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
-            templateUrl: 'documents.component.html'
+            templateUrl: 'documents.component.html',
+            styleUrls: ['documents.component.css']
         })
     ], DocumentsComponent);
     return DocumentsComponent;
--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -4,31 +4,33 @@ import { Document } from './document';
 @Component({
   moduleId: module.id,
   selector: 'documents',
-  templateUrl: 'documents.component.html'
+  templateUrl: 'documents.component.html',
+  styleUrls: ['documents.component.css']
 })
 export class DocumentsComponent {
   pageTitle: string = "Document Dashboard"
+
   documents: Document[] = [
-  {
-    title: "My first doc",
-    description: 'this is my first doc',
-    file_url: 'http://google.com',
-    updated_at: '02/01/18',
-    image_url: 'http://google.com'
-  },
-  {
-    title: "My Second doc",
-    description: 'this is my first doc',
-    file_url: 'http://google.com',
-    updated_at: '02/01/18',
-    image_url: 'http://google.com'
-  },
-  {
-    title: "My Third doc",
-    description: 'this is my first doc',
-    file_url: 'http://google.com',
-    updated_at: '02/01/18',
-    image_url: 'http://google.com'
-  }
+    {
+      title: "My First Doc",
+      description: 'asdfasdfasdf asdfasd',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+    },
+    {
+      title: "My Second Doc",
+      description: 'asdfasdfasdf asdfasd',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+    },
+    {
+      title: "My Last Doc",
+      description: 'asdfasdfasdf asdfasd',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+    }
   ]
 }
